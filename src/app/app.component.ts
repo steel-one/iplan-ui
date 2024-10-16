aa0921634b9a0f3c81b5b679bce05cf5dbd70b75
ViacheslavDenisenko <vdenisenko@centrak.com>
2024-10-16 11:47:13 +0400

    #4&9: ng g @angular/core:standalone Bootstrap the project using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { NgIf, AsyncPipe } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        NgIf,
+        RouterOutlet,
+        AsyncPipe,
+    ],
 })
 export class AppComponent {
   constructor(public srv: AppService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,38 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+import { AppUpdaterService } from './app/app-updater.service';
+import { HttpErrorHandler } from './app/error.handler';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './app/auth/auth.interceptor';
+import { authStrategyProvider } from './app/auth/services/auth.strategy';
+import { AppService } from './app/app.service';
+import { httpErrorInterceptor } from './app/error.interceptor';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { isDevMode, importProvidersFrom } from '@angular/core';
+import { SidebarModule } from './app/sidebar/sidebar.module';
+import { AuthModule } from './app/auth/auth.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerImmediately',
+        }), SidebarModule, AuthModule, AppRoutingModule),
+        AppUpdaterService,
+        HttpErrorHandler,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        },
+        authStrategyProvider,
+        AppService,
+        provideHttpClient(withInterceptors([httpErrorInterceptor])),
+        provideAnimations(),
+    ]
+})
   .catch((err) => console.error(err));
